a4cd76f4aea6e8177b3fca3f6d372a652edb9362
Tyler Coy <tylercoy@utexas.edu>
2020-06-08 20:06:49 -0500

    Replace ARM assembly

--- inc/util.h
+++ inc/util.h
@@ -23,22 +23,14 @@
 #ifndef QPSK_UTIL_H_
 #define QPSK_UTIL_H_
 
-#ifndef __arm__
 #include <cmath>
-#endif
 
 namespace qpsk
 {
 
 inline float Abs(float x)
 {
-#ifdef __arm__
-    float out;
-    asm("vabs.f32 %0, %1" : "=t" (out) : "t" (x));
-    return out;
-#else
     return std::fabs(x);
-#endif
 }
 
 template <typename T>
@@ -49,15 +41,7 @@ inline T Clamp(T x, T min, T max)
 
 inline float Truncate(float x)
 {
-#ifdef __arm__
-    float y;
-    // The vcvt instruction rounds toward zero
-    asm("vcvt.s32.f32 %0, %1" : "=t" (y) : "t" (x));
-    asm("vcvt.f32.s32 %0, %0" : "+t" (y));
-    return y;
-#else
-    return std::truncf(x);
-#endif
+    return static_cast<float>(static_cast<int>(x));
 }
 
 inline float FractionalPart(float x)
