3c322be40a5046e9665ab87ecfca623aaddb1e1c
Tyler Coy <tylercoy@utexas.edu>
2020-06-10 00:13:06 -0500

    Refactor encoder
    Added options for specifying the target's flash page layout

--- encoder.py
+++ encoder.py
@@ -6,6 +6,7 @@
 # Copyright 2020 Tyler Coy
 #
 # Author: Emilie Gillet (emilie.o.gillet@gmail.com)
+# Author: Tyler Coy (alrightdevices.com)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -34,7 +35,6 @@
 import argparse
 import zlib
 import wave
-from intelhex import IntelHex
 import os.path
 import struct
 import array
@@ -42,27 +42,32 @@ import math
 import sys
 import string
 import io
+import itertools
 
 
 
 class QPSKEncoder():
 
     def __init__(self, sample_rate, symbol_rate,
-                page_size, packet_size, crc_seed, page_write_time):
+                packet_size, block_size, write_time,
+                flash_spec, reserved_size,
+                crc_seed, fill_byte):
         assert (sample_rate % symbol_rate) == 0
-        assert (page_size % packet_size) == 0
         assert (packet_size % 4) == 0
+        assert (block_size % packet_size) == 0
 
         self._sample_rate = sample_rate
         self._symbol_rate = symbol_rate
-        self._page_size = page_size
         self._packet_size = packet_size
+        self._block_size = block_size
+        self._write_time = write_time
+        self._flash_spec = flash_spec
+        self._reserved_size = reserved_size
         self._crc_seed = crc_seed
+        self._fill_byte = struct.pack('B', fill_byte)
 
         self._packet_preamble = b'\x00' * 8 + b'\x99' * 4 + b'\xCC' * 4
         self._samples_per_symbol = self._sample_rate // self._symbol_rate
-        self._packets_per_page = self._page_size // self._packet_size
-        self._page_write_time = page_write_time
 
         self._signal = array.array('h')
         self._symbol_lookup = self._construct_symbols()
@@ -107,17 +112,40 @@ class QPSKEncoder():
         for byte in self._packet_preamble + data + struct.pack('>L', crc):
             self._encode_byte(byte)
 
+    def _page_spec(self, flash_spec):
+        for (size, time, num) in flash_spec:
+            if num == None:
+                yield from itertools.repeat((size, time))
+            else:
+                yield from itertools.repeat((size, time), num)
+
     def encode(self, data):
-        assert (len(data) % self._page_size) == 0
+        page = self._page_spec(self._flash_spec)
+
+        while self._reserved_size > 0:
+            (page_size, erase_time) = page.__next__()
+            self._reserved_size -= page_size
+        assert self._reserved_size == 0
+
+        while len(data) % self._block_size:
+            data += self._fill_byte
 
         self._encode_intro()
 
-        num_pages = len(data) // self._page_size
-        for page in range(num_pages):
-            for packet in range(self._packets_per_page):
-                offset = page * self._page_size + packet * self._packet_size
-                self._encode_packet(data[offset:(offset + self._packet_size)])
-            self._encode_blank(self._page_write_time)
+        while len(data) > 0:
+            (page_size, erase_time) = page.__next__()
+            remaining = min(page_size, len(data))
+            assert (remaining % self._block_size) == 0
+
+            for i in range(remaining // self._block_size):
+                for j in range(self._block_size // self._packet_size):
+                    packet = data[:self._packet_size]
+                    assert len(packet) == self._packet_size
+                    data = data[self._packet_size:]
+                    self._encode_packet(packet)
+                if i == 0:
+                    self._encode_blank(erase_time)
+                self._encode_blank(self._write_time)
 
         self._encode_outro()
 
@@ -131,44 +159,65 @@ def main():
     parser.add_argument('-s', '--sample-rate', dest='sample_rate',
         type=int,
         required=True,
-        help='Sample rate in Hz (must be a multiple of the symbol rate).')
-    parser.add_argument('-c', '-y', '--carrier-frequency', '--symbol-rate',
-        dest='symbol_rate',
+        help='Sample rate in Hz. Must be a multiple of the symbol rate.')
+    parser.add_argument('-y', '--symbol-rate', dest='symbol_rate',
         type=int,
         required=True,
         help='Symbol rate in Hz.')
+    parser.add_argument('-b', '--block-size', dest='block_size',
+        required=True,
+        help='The number of bytes that the target will write at a time. '
+            'Must be a multiple of the packet size.')
+    parser.add_argument('-w', '--write-time', dest='write_time',
+        required=True,
+        help='Amount of time in milliseconds required by the target to write '
+            'a block.')
+    parser.add_argument('-f', '--flash-spec', dest='flash_spec',
+        required=True,
+        nargs='+',
+        help=
+            'Flash page specification which describes the layout of the '
+            'target\'s flash memory. This is a list of '
+            'specifiers of the form "SIZE:ETIME[:NUM]", where SIZE is the '
+            'size in bytes of the page (and must be a multiple of the block '
+            'size), ETIME is the amount of time in milliseconds to allow for '
+            'the page erase to complete, and NUM optionally specifies the '
+            'number of these pages (if NUM is not given, then this specifier '
+            'will be used for all remaining pages). For example, a simple '
+            'layout consisting of 1KB pages that require 50ms each to erase '
+            'could be specified as "1024:50", while a more complex layout '
+            'like that of the STM32F405 could be specified as '
+            '"16K:500:4 64K:1100:1 128K:2000:7".')
+    parser.add_argument('-a', '--start-address', dest='start_address',
+        required=True,
+        help='Flash memory address at which the firmware will be written. '
+            'If a "+" is prepended, this will be interpreted as an offset from '
+            'the base address, otherwise as an absolute address. Must align '
+            'to a page.')
+    parser.add_argument('-x', '--base-address', dest='base_address',
+        default='0',
+        help='Base address of the target\'s flash memory. Default 0.')
     parser.add_argument('-p', '--packet-size', dest='packet_size',
         default='256',
-        help='Packet size in bytes, default 256 (must be a multiple of 4).')
-    parser.add_argument('-f', '--page-size', dest='page_size',
-        type=int,
-        required=True,
-        help='Flash page size in bytes (must be a multiple of the packet size')
-    parser.add_argument('-w', '--page-write-time', dest='page_write_time',
-        required=True,
-        help='Flash page write time in seconds')
-    parser.add_argument('-x', '--offset', dest='file_offset',
-        default='0',
-        help='Ignore hex file contents before this location.')
-    parser.add_argument('-b', '--base', dest='file_base',
-        default='0',
-        help='FILE_OFFSET is relative to this address.')
-    parser.add_argument('-e', '--seed', dest='crc_seed',
-        default='0',
-        help='CRC32 seed, default 0.')
-    parser.add_argument('-t', '--file-type', dest='file_type',
-        choices=['hex', 'bin', 'auto'], default='auto',
-        help='Input file type, default auto.')
+        help='Packet size in bytes. Must be a multiple of 4. Default 256.')
     parser.add_argument('--fill', dest='fill_byte',
         default='0xFF',
-        help='Fill/pad byte (default 0xFF).')
+        help='Byte value to use to fill gaps and pad lengths. Default 0xFF.')
+    parser.add_argument('-e', '--seed', dest='crc_seed',
+        default='0',
+        help='CRC32 seed. Default 0.')
+    parser.add_argument('-t', '--file-type', dest='file_type',
+        choices=['hex', 'bin', 'auto'], default='auto',
+        help='Input file type. If a hex file is used, all '
+            'content before the start address will be ignored. A bin file is '
+            'always assumed to start at the start address. Default auto.')
     parser.add_argument('-i', '--input-file', dest='input_file',
         default='-',
-        help='Input file (bin or hex), default stdin.')
+        help='Input file (bin or hex). Default stdin.')
     parser.add_argument('-o', '--output-file', dest='output_file',
         default=None,
-        help='Output wav file, default derived from input file name if given, '
-            'else stdout.')
+        help='Output wav file. The default is derived from the input file name '
+            'if one is given, otherwise stdout.')
     args = parser.parse_args()
 
     fill_byte = int(args.fill_byte, 0)
@@ -193,10 +242,15 @@ def main():
         is_hex = all(map(lambda x: chr(x) in string.hexdigits + ':\r\n', data))
         args.file_type = 'hex' if is_hex else 'bin'
 
+    base_address = int(args.base_address, 0)
+    if args.start_address.startswith('+'):
+        start_address = base_address + int(args.start_address[1:], 0)
+    else:
+        start_address = int(args.start_address, 0)
+
     if args.file_type == 'hex':
-        offset = int(args.file_offset, 0)
-        base = int(args.file_base, 0)
-        ihex = IntelHex(io.StringIO(data.decode('ascii')))[base+offset:]
+        from intelhex import IntelHex
+        ihex = IntelHex(io.StringIO(data.decode('ascii')))[start_address:]
         ihex.padding = fill_byte
         data = ihex.tobinstr()
 
@@ -208,14 +262,13 @@ def main():
     encoder = QPSKEncoder(
             sample_rate = args.sample_rate,
             symbol_rate = args.symbol_rate,
-            page_size   = args.page_size,
             crc_seed    = int(args.crc_seed, 0),
-            packet_size = int(args.packet_size, 0),
-            page_write_time = float(args.page_write_time))
-
-    fill_byte = struct.pack('B', fill_byte)
-    while len(data) % args.page_size:
-        data += fill_byte
+            packet_size = parse_size(args.packet_size),
+            block_size  = parse_size(args.block_size),
+            write_time  = float(args.write_time) / 1000,
+            flash_spec  = parse_flash_spec(args.flash_spec),
+            reserved_size = start_address - base_address,
+            fill_byte   = fill_byte)
 
     encoder.encode(data)
 
@@ -225,6 +278,29 @@ def main():
     writer.setnchannels(1)
     writer.writeframes(encoder.tobytes())
     writer.close()
+
+def parse_size(size):
+    if size.upper().endswith('K'):
+        return int(size[:-1], 0) * 1024
+    else:
+        return int(size, 0)
+
+def parse_flash_spec(spec_list):
+    parsed_list = []
+    for spec in spec_list:
+        spec = spec.split(':')
+        try:
+            size, time, num = spec
+            num = int(num, 0)
+        except ValueError:
+            size, time = spec
+            num = None
+
+        size = parse_size(size)
+        time = float(time) / 1000
+
+        parsed_list.append((size, time, num))
+    return parsed_list
 
 if __name__ == '__main__':
     main()
