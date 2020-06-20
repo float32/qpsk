54628fbdeab4fb75d8a6fe454a29c6185ef80d38
Tyler Coy <tylercoy@utexas.edu>
2020-06-19 23:36:05 -0500

    Refactor encoder

--- encoder.py
+++ encoder.py
@@ -46,147 +46,6 @@ import itertools
 
 
 
-class QPSKEncoder():
-
-    def __init__(self, sample_rate, symbol_rate,
-                packet_size, block_size, write_time,
-                flash_spec, reserved_size,
-                crc_seed, fill_byte):
-        assert (sample_rate % symbol_rate) == 0
-        assert (packet_size % 4) == 0
-        assert (block_size % packet_size) == 0
-
-        self._sample_rate = sample_rate
-        self._symbol_rate = symbol_rate
-        self._packet_size = packet_size
-        self._block_size = block_size
-        self._write_time = write_time
-        self._flash_spec = flash_spec
-        self._reserved_size = reserved_size
-        self._crc_seed = crc_seed
-        self._fill_byte = struct.pack('B', fill_byte)
-
-        self._alignment_sequence = b'\x99' * 4
-        self._block_marker = b'\xCC\xCC\xCC\xCC'
-        self._end_marker = b'\xF0\xF0\xF0\xF0'
-        self._samples_per_symbol = self._sample_rate // self._symbol_rate
-
-        self._signal = array.array('h')
-        self._symbol_lookup = self._construct_symbols()
-
-    def _construct_symbols(self):
-        lookup = list()
-        for symbol in range(4):
-            msb = (symbol & 2) - 1
-            lsb = (symbol & 1) * 2 - 1
-            samples = list()
-            for i in range(self._samples_per_symbol):
-                phase = 2 * math.pi * i / self._samples_per_symbol
-                sample = (msb * math.cos(phase) - lsb * math.sin(phase))
-                sample /= math.sqrt(2)
-                assert (sample >= -1) and (sample <= 1)
-                samples.append(int(32767 * sample))
-            lookup.append(array.array('h', samples))
-        return lookup
-
-    def _encode_symbol(self, symbol):
-        self._signal.extend(self._symbol_lookup[symbol])
-
-    def _encode_blank(self, duration):
-        length = int(duration * self._symbol_rate)
-        self._signal.extend(self._symbol_lookup[0] * length)
-
-    def _encode_intro(self):
-        self._signal.extend([0] * (self._sample_rate // 10))
-        self._encode_blank(1.0)
-
-    def _encode_resync(self):
-        self._encode_blank(0.025) # PLL resync
-
-    def _encode_outro(self):
-        self._encode_resync()
-        for byte in self._alignment_sequence + self._end_marker:
-            self._encode_byte(byte)
-        self._signal.extend([0] * (self._sample_rate // 10))
-
-    def _encode_byte(self, byte):
-        self._encode_symbol((byte >> 6) & 3);
-        self._encode_symbol((byte >> 4) & 3);
-        self._encode_symbol((byte >> 2) & 3);
-        self._encode_symbol((byte >> 0) & 3);
-
-    def _hamming(self, data):
-        parity = 0
-        bit_num = 1
-        for i in range(len(data) * 8):
-            while ((bit_num & (bit_num - 1)) == 0):
-                bit_num += 1
-            bit = (data[i // 8] >> (i % 8)) & 1
-            if bit:
-                parity ^= bit_num
-            bit_num += 1
-        return parity
-
-    def _encode_packet(self, data):
-        assert len(data) == self._packet_size
-        crc = zlib.crc32(data, self._crc_seed) & 0xFFFFFFFF
-        data += struct.pack('<L', crc)
-        for byte in data + struct.pack('<H', self._hamming(data)):
-            self._encode_byte(byte)
-
-    def _encode_block(self, data):
-        assert len(data) == self._block_size
-
-        self._encode_resync()
-
-        for byte in self._alignment_sequence + self._block_marker:
-            self._encode_byte(byte)
-
-        for i in range(0, self._block_size, self._packet_size):
-            packet = data[i : i + self._packet_size]
-            self._encode_packet(packet)
-
-
-    def _page_spec(self, flash_spec):
-        for (size, time, num) in flash_spec:
-            if num == None:
-                yield from itertools.repeat((size, time))
-            else:
-                yield from itertools.repeat((size, time), num)
-
-    def encode(self, data):
-        page = self._page_spec(self._flash_spec)
-
-        while self._reserved_size > 0:
-            (page_size, erase_time) = page.__next__()
-            self._reserved_size -= page_size
-        assert self._reserved_size == 0
-
-        while len(data) % self._block_size:
-            data += self._fill_byte
-
-        self._encode_intro()
-
-        while len(data) > 0:
-            (page_size, erase_time) = page.__next__()
-            remaining = min(page_size, len(data))
-            assert (remaining % self._block_size) == 0
-
-            for i in range(remaining // self._block_size):
-                block = data[:self._block_size]
-                data = data[self._block_size:]
-                self._encode_block(block)
-                if i == 0:
-                    self._encode_blank(erase_time)
-                self._encode_blank(self._write_time)
-
-        self._encode_outro()
-
-    def tobytes(self):
-        return self._signal.tobytes()
-
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--sample-rate', dest='sample_rate',
@@ -253,8 +112,6 @@ def main():
             'if one is given, otherwise stdout.')
     args = parser.parse_args()
 
-    fill_byte = int(args.fill_byte, 0)
-
     if args.input_file == '-':
         input_file = sys.stdin.buffer
         if args.output_file == None:
@@ -281,6 +138,8 @@ def main():
     else:
         start_address = int(args.start_address, 0)
 
+    fill_byte = int(args.fill_byte, 0)
+
     if args.file_type == 'hex':
         from intelhex import IntelHex
         ihex = IntelHex(io.StringIO(data.decode('ascii')))[start_address:]
@@ -292,25 +151,171 @@ def main():
     else:
         output_file = args.output_file
 
-    encoder = QPSKEncoder(
-            sample_rate = args.sample_rate,
-            symbol_rate = args.symbol_rate,
-            crc_seed    = int(args.crc_seed, 0),
-            packet_size = parse_size(args.packet_size),
-            block_size  = parse_size(args.block_size),
-            write_time  = float(args.write_time) / 1000,
-            flash_spec  = parse_flash_spec(args.flash_spec),
+    arrangement = Arrangement(
+            flash_spec    = args.flash_spec,
             reserved_size = start_address - base_address,
-            fill_byte   = fill_byte)
+            block_size    = parse_size(args.block_size),
+            fill_byte     = fill_byte,
+            write_time    = float(args.write_time),
+            data          = data)
 
-    encoder.encode(data)
+    encoder = Encoder(
+            symbol_rate = args.symbol_rate,
+            packet_size = parse_size(args.packet_size),
+            crc_seed    = int(args.crc_seed, 0))
+
+    symbols = encoder.encode(arrangement)
+
+    assert (args.sample_rate % args.symbol_rate) == 0
+    modulator = QPSKModulator(args.sample_rate, args.symbol_rate)
+    signal = modulator.modulate(symbols)
 
     writer = wave.open(output_file, 'wb')
     writer.setframerate(args.sample_rate)
     writer.setsampwidth(2)
     writer.setnchannels(1)
-    writer.writeframes(encoder.tobytes())
+    writer.writeframes(signal.tobytes())
     writer.close()
+
+
+
+class Arrangement:
+    def __init__(self, flash_spec, reserved_size,
+                block_size, fill_byte, write_time, data):
+        self._blocks = []
+        for (erase_time, page_data) in Pages(data, flash_spec, reserved_size):
+            for block_data in Blocks(page_data, block_size, fill_byte):
+                wait_time = (erase_time + write_time) / 1000
+                self._blocks.append((block_data, wait_time))
+
+    def __iter__(self):
+        return iter(self._blocks)
+
+
+
+class Encoder:
+
+    def __init__(self, symbol_rate, packet_size, crc_seed):
+        assert (packet_size % 4) == 0
+
+        self._symbol_rate = symbol_rate
+        self._packet_size = packet_size
+        self._crc_seed = crc_seed
+
+        self._alignment_sequence = b'\x99' * 4
+        self._block_marker = b'\xCC\xCC\xCC\xCC'
+        self._end_marker = b'\xF0\xF0\xF0\xF0'
+
+        self._byte_table = []
+        for byte in range(256):
+            self._byte_table.append([
+                ((byte >> 6) & 3),
+                ((byte >> 4) & 3),
+                ((byte >> 2) & 3),
+                ((byte >> 0) & 3)])
+
+        self._hamming_table = []
+        bit_num = 1
+        for i in range((packet_size + 4) * 8):
+            while ((bit_num & (bit_num - 1)) == 0):
+                bit_num += 1
+            self._hamming_table.append((bit_num, i // 8, 1 << (i % 8)))
+            bit_num += 1
+
+    def _encode_blank(self, duration):
+        return [0] * int(duration * self._symbol_rate)
+
+    def _encode_intro(self):
+        return self._encode_blank(1.0)
+
+    def _encode_resync(self):
+        return self._encode_blank(0.025) # PLL resync
+
+    def _encode_outro(self):
+        symbols = self._encode_resync()
+        for byte in self._alignment_sequence + self._end_marker:
+            symbols += self._encode_byte(byte)
+        return symbols
+
+    def _encode_byte(self, byte):
+        return self._byte_table[byte]
+
+    def _hamming(self, data):
+        parity = 0
+        for (bit_num, byte_num, mask) in self._hamming_table:
+            if data[byte_num] & mask:
+                parity ^= bit_num
+        return parity
+
+    def _encode_packet(self, data):
+        assert len(data) == self._packet_size
+        crc = zlib.crc32(data, self._crc_seed) & 0xFFFFFFFF
+        data += struct.pack('<L', crc)
+        symbols = []
+        for byte in data + struct.pack('<H', self._hamming(data)):
+            symbols += self._encode_byte(byte)
+        return symbols
+
+    def _encode_block(self, data):
+        assert (len(data) % self._packet_size) == 0
+
+        symbols = self._encode_resync()
+
+        for byte in self._alignment_sequence + self._block_marker:
+            symbols += self._encode_byte(byte)
+
+        for i in range(0, len(data), self._packet_size):
+            packet = data[i : i + self._packet_size]
+            symbols += self._encode_packet(packet)
+
+        return symbols
+
+    def encode(self, blocks):
+        symbols = []
+        symbols += self._encode_intro()
+
+        for (data, time) in blocks:
+            symbols += self._encode_block(data)
+            symbols += self._encode_blank(time)
+
+        symbols += self._encode_outro()
+        return symbols
+
+
+
+class QPSKModulator:
+
+    def __init__(self, sample_rate, symbol_rate):
+        assert (sample_rate % symbol_rate) == 0
+        symbol_duration = sample_rate // symbol_rate
+        self._sample_rate = sample_rate
+        self._symbol_table = self._construct_symbols(symbol_duration)
+
+    def _construct_symbols(self, symbol_duration):
+        lookup = list()
+        for symbol in range(4):
+            msb = (symbol & 2) - 1
+            lsb = (symbol & 1) * 2 - 1
+            samples = list()
+            for i in range(symbol_duration):
+                phase = 2 * math.pi * i / symbol_duration
+                sample = (msb * math.cos(phase) - lsb * math.sin(phase))
+                sample /= math.sqrt(2)
+                assert (sample >= -1) and (sample <= 1)
+                samples.append(int(32767 * sample))
+            lookup.append(array.array('h', samples))
+        return lookup
+
+    def modulate(self, symbols):
+        signal = array.array('h')
+        silence = [0] * (self._sample_rate // 10)
+        signal.extend(silence)
+        for symbol in symbols:
+            signal.extend(self._symbol_table[symbol])
+        signal.extend(silence)
+        return signal
+
+
 
 def parse_size(size):
     if size.upper().endswith('K'):
@@ -318,10 +323,33 @@ def parse_size(size):
     else:
         return int(size, 0)
 
-def parse_flash_spec(spec_list):
-    parsed_list = []
-    for spec in spec_list:
-        spec = spec.split(':')
+class Pages:
+    def __init__(self, data, flash_spec, reserved_size):
+        page_spec = self._page_specs(flash_spec)
+
+        while reserved_size > 0:
+            (page_size, erase_time) = next(page_spec)
+            reserved_size -= page_size
+        assert reserved_size == 0
+
+        self._pages = []
+        while len(data) > 0:
+            (page_size, erase_time) = next(page_spec)
+            self._pages.append((erase_time, data[:page_size]))
+            data = data[page_size:]
+
+    def __iter__(self):
+        return iter(self._pages)
+
+    def _page_specs(self, flash_spec):
+        for (size, time, num) in map(self._parse_page_spec, flash_spec):
+            if num == None:
+                yield from itertools.repeat((size, time))
+            else:
+                yield from itertools.repeat((size, time), num)
+
+    def _parse_page_spec(self, page_spec):
+        spec = page_spec.split(':')
         try:
             size, time, num = spec
             num = int(num, 0)
@@ -330,10 +358,22 @@ def parse_flash_spec(spec_list):
             num = None
 
         size = parse_size(size)
-        time = float(time) / 1000
+        time = float(time)
+        return (size, time, num)
 
-        parsed_list.append((size, time, num))
-    return parsed_list
+class Blocks:
+    def __init__(self, data, block_size, fill_byte):
+        if len(data) % block_size:
+            padding = block_size - (len(data) % block_size)
+            data += bytes([fill_byte]) * padding
+        assert len(data) % block_size == 0
+        self._blocks = [data[i : i + block_size]
+            for i in range(0, len(data), block_size)]
+
+    def __iter__(self):
+        return iter(self._blocks)
+
+
 
 if __name__ == '__main__':
     main()
